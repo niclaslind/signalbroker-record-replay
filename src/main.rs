3aa3bad1ef1e2b57b8d7312ea2c6cff6f4290995
Lind, Niclas <niclas.lind@volvocars.com>
2020-08-30 15:28:34 +0200

    Simple gRPC client, sub/pub to signalbroker-server

---
+++ src/main.rs
@@ -0,0 +1,101 @@
+pub mod protos;
+
+use grpcio::{ChannelBuilder, EnvBuilder, ClientSStreamReceiver};
+use std::sync::{Arc, Mutex};
+use protobuf::{SingularPtrField, RepeatedField};
+use std::thread;
+use std::time::Duration;
+use futures::Stream;
+
+fn main() {
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect(format!("localhost:50051").as_str());
+    let client =
+        Arc::new(Mutex::new(protos::network_api_grpc::NetworkServiceClient::new(ch)));
+
+    let sub_client = Arc::clone(&client);
+    let subscribe = thread::spawn(move || {
+        let client = sub_client.lock().unwrap();
+        subscribe_to_signals(&client);
+    });
+
+    let pub_client = Arc::clone(&client);
+    let publish = thread::spawn(move || loop {
+        let client = pub_client.lock().unwrap();
+        publish_signals(&client);
+        thread::sleep(Duration::from_millis(500));
+    });
+
+    subscribe.join().expect("The receiver thread has panicked");
+    publish.join().expect("The sender thread has panicked");
+}
+
+fn subscribe_to_signals(client: &protos::network_api_grpc::NetworkServiceClient) {
+    let mut client_id = protos::common::ClientId::new();
+    client_id.id = "rusty_client_sub".to_string();
+
+    let mut subscriber_config = protos::network_api::SubscriberConfig::new();
+    subscriber_config.clientId = SingularPtrField::some(client_id);
+    subscriber_config.signals = generate_signal_ids();
+
+    let sub_info: ClientSStreamReceiver<protos::network_api::Signals> = client.subscribe_to_signals(&subscriber_config).unwrap();
+
+    match sub_info.poll_next() {}
+}
+
+// Publish signals to signal-broker over gRPC
+fn publish_signals(client: &protos::network_api_grpc::NetworkServiceClient) {
+    let mut client_id = protos::common::ClientId::new();
+    client_id.id = "rusty_client_pub".to_string();
+
+    let mut publisher_config = protos::network_api::PublisherConfig::new();
+    publisher_config.clientId = SingularPtrField::some(client_id);
+    publisher_config.signals = generate_signals();
+    let _result = client.publish_signals(&publisher_config).unwrap();
+}
+
+fn generate_signal_ids() -> SingularPtrField<protos::network_api::SignalIds> {
+    let mut namespace = protos::common::NameSpace::new();
+    namespace.name = "VirtualInterface".to_string();
+
+    let mut signal_id = protos::common::SignalId::new();
+    signal_id.name = "virtual_signal".to_string();
+    signal_id.namespace = SingularPtrField::some(namespace);
+
+    let signal_ids_vector = vec![signal_id];
+
+    let mut signal_ids = protos::network_api::SignalIds::new();
+    signal_ids.signalId = RepeatedField::from_vec(signal_ids_vector);
+
+    SingularPtrField::some(signal_ids)
+}
+
+// Generate signals from proto_buf files
+fn generate_signals() -> SingularPtrField<protos::network_api::Signals> {
+    let mut namespace = protos::common::NameSpace::new();
+    namespace.name = "VirtualInterface".to_string();
+
+    let mut signal_id = protos::common::SignalId::new();
+    signal_id.name = "virtual_signal".to_string();
+    signal_id.namespace = SingularPtrField::some(namespace);
+
+    // return the time_now - UNIX_EPOCH
+    let payload = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("Something went wrong")
+        .as_secs() as i64;
+
+    let signal_payload = Option::from(protos::network_api::Signal_oneof_payload::integer(payload));
+
+    let mut signal = protos::network_api::Signal::new();
+    signal.id = SingularPtrField::some(signal_id);
+    signal.payload = signal_payload;
+
+    let signals_vector = vec![signal];
+
+    let mut signals = protos::network_api::Signals::new();
+    signals.signal = RepeatedField::from_vec(signals_vector);
+
+    SingularPtrField::some(signals)
+}
+
