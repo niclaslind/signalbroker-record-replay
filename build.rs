685e547a4e82fe9c80b8609504c98918c4af6630
Lind, Niclas <niclas.lind@volvocars.com>
2020-08-30 15:27:51 +0200

    Init commit

--- build.rs
+++ build.rs
@@ -1,1 +1,13 @@
-extern crate 
+extern crate protoc_grpcio;
+
+fn main() {
+    let proto_root = "src/protos";
+    println!("cargo:rerun-if-changed={}", proto_root);
+    protoc_grpcio::compile_grpc_protos(
+        &["common.proto", "diagnostics_api.proto",
+            "functional_api.proto", "network_api.proto", "system_api.proto"],
+        &[proto_root],
+        &proto_root,
+        None,
+    ).expect("Failed to compile gRPC definitions!")
+}
---
+++ src/protos/mod.rs
@@ -0,0 +1,13 @@
+extern crate grpcio;
+extern crate protobuf;
+extern crate futures;
+
+pub mod common;
+pub mod diagnostics_api;
+pub mod diagnostics_api_grpc;
+pub mod functional_api;
+pub mod functional_api_grpc;
+pub mod network_api;
+pub mod network_api_grpc;
+pub mod system_api;
+pub mod system_api_grpc;
